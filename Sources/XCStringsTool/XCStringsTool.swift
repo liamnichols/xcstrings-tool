663294009fbc88e19f714b3a64f68b7f8e98b652
Liam Nichols <liam.nichols.ln@gmail.com>
2023-09-25 10:36:20 +0200

    Change version to 0.0.1

--- Sources/XCStringsTool/XCStringsTool.swift
+++ Sources/XCStringsTool/XCStringsTool.swift
@@ -5,7 +5,7 @@ struct XCStringsTool: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "xcstrings-tool",
         abstract: "Generates Swift code from String Catalogs (.xcstrings files)",
-        version: "0.1.0",
+        version: "0.0.1",
         subcommands: [GenerateCommand.self],
         defaultSubcommand: GenerateCommand.self
     )
