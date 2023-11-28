e73c640848cd777d7aaf38e89536bc776141b569
Paul Gessinger <hello@paulgessinger.com>
2023-11-28 10:48:35 +0100

    Add cases for %u, %x and %o to produce `UInt` arguments (#23)

--- Sources/StringExtractor/PlaceholderType.swift
+++ Sources/StringExtractor/PlaceholderType.swift
@@ -1,6 +1,6 @@
 // https://github.com/SwiftGen/SwiftGen/blob/stable/Sources/SwiftGenKit/Parsers/Strings/PlaceholderType.swift
 enum PlaceholderType {
-    case object, float, int, char, cString, pointer
+    case object, float, int, uint, char, cString, pointer
 
     /// Create an instance using a formatSpecifier 
     init?(formatSpecifier: some StringProtocol) {
@@ -10,8 +10,10 @@ enum PlaceholderType {
             self = .object
         case "a", "e", "f", "g":
             self = .float
-        case "d", "i", "o", "u", "x":
+        case "d", "i":
             self = .int
+        case "u", "x", "X", "o":
+            self = .uint
         case "c":
             self = .char
         case "s":
@@ -28,6 +30,7 @@ enum PlaceholderType {
         case .object: "String"
         case .float: "Double"
         case .int: "Int"
+        case .uint: "UInt"
         case .char: "CChar"
         case .cString: "UnsafePointer<CChar>"
         case .pointer: "UnsafeRawPointer"
