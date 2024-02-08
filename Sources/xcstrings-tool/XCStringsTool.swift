ecd11e1efd7ccb74236f0c0b99ac6fb5246a9c9d
Liam Nichols <liam.nichols.ln@gmail.com>
2024-02-08 16:07:03 +0000

    Bump version to 0.1.2

--- Sources/xcstrings-tool/XCStringsTool.swift
+++ Sources/xcstrings-tool/XCStringsTool.swift
@@ -5,7 +5,7 @@ struct XCStringsTool: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "xcstrings-tool",
         abstract: "Generates Swift code from String Catalogs (.xcstrings files)",
-        version: "0.1.1",
+        version: "0.1.2",
         subcommands: [Generate.self],
         defaultSubcommand: Generate.self
     )
