e9672247b99e9aa2741a73b27792b6923b2b69f3
Liam Nichols <liam.nichols.ln@gmail.com>
2024-05-28 22:09:29 +0200

    Update CI (#89)
    * Add Xcode 15.4 to the test matrix
    
    * Update actions
    
    * Support building binary using workflow_dispatch
    
    * Compile snapshots as part of CI checks

---
+++ .github/scripts/compile-snapshots.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Swift Version:"
+xcrun swift --version
+echo ""
+
+for snapshot in Tests/XCStringsToolTests/__Snapshots__/**/*.swift; do
+  echo "Compiling ‘$(basename $snapshot)‘"
+
+  if xcrun swiftc "$snapshot" -package-name "MyPackage" -o /dev/null ; then
+    echo "  Success"
+  fi
+done
