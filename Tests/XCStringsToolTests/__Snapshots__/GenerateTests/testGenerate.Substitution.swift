5296dbd64066bd3ac49c3d2e0f0a1717af596ff5
Liam Nichols <liam.nichols.ln@gmail.com>
2024-05-30 23:41:56 +0200

    Bump version to 0.4.2

--- Tests/XCStringsToolTests/__Snapshots__/GenerateTests/testGenerate.Substitution.swift
+++ Tests/XCStringsToolTests/__Snapshots__/GenerateTests/testGenerate.Substitution.swift
@@ -46,7 +46,7 @@ extension String {
     /// }
     /// ```
     ///
-    /// - SeeAlso: [XCStrings Tool Documentation - Using the generated source code](https://swiftpackageindex.com/liamnichols/xcstrings-tool/0.4.1/documentation/documentation/using-the-generated-source-code)
+    /// - SeeAlso: [XCStrings Tool Documentation - Using the generated source code](https://swiftpackageindex.com/liamnichols/xcstrings-tool/0.4.2/documentation/documentation/using-the-generated-source-code)
     internal struct Substitution: Sendable {
         enum BundleDescription: Sendable {
             case main
